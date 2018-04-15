f73731c8a1e533b42ceac61db644a37579da8665
qvolcano <qvolcano@126.com>
2018-04-16 02:42:08 +0800

    update

--- src/com/qvolcano/mcsp/PluginLoader.java
+++ src/com/qvolcano/mcsp/PluginLoader.java
@@ -2,13 +2,25 @@ package com.qvolcano.mcsp;
 
 import java.io.File;
 
+import com.qvolcano.io.FileLoader;
+import com.qvolcano.mcsp.js.JSPlugin;
+
 public class PluginLoader {
 	private Plugin plugin;
+	private FileLoader loader=new FileLoader();
 	
 	public void load(String path) {load(new File(path));}
 	public void load(File file) {
 		if(file.exists()) {
 			
+			String content = loader.loadString(file);
+			if(content!="") {
+				String filename=file.getName();
+				String filetype=filename.substring(filename.lastIndexOf("."),filename.length());
+				if(filetype==".js") {
+					plugin=new JSPlugin(content);
+				}
+			}
 		}
 	}
 	
--- src/com/qvolcano/mcsp/js/JSPlugin.java
+++ src/com/qvolcano/mcsp/js/JSPlugin.java
@@ -1,17 +1,39 @@
 package com.qvolcano.mcsp.js;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 import com.qvolcano.mcsp.Plugin;
 
 public class JSPlugin extends Plugin {
-	String script_text="";
+	String source="";
+	Object script=null;
 	
+	public JSPlugin(String source) {
+		this.source=source;
+	}
+
 	@Override
 	public void onEnable() {
+		this.script=createScript();
 		
 	}
 	
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	private Object createScript() {
+		Object script=null;
+		try {
+			Context context=Context.enter();
+			Scriptable scope=context.initStandardObjects(null);
+			context.evaluateString(scope, source, "script", 1, null);
+			script=scope.get("script", scope);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return script;
 	}
 }
